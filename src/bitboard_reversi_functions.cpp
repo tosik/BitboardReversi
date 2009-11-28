7260ee6e4a73348b1e929316b6d1443dbfa59ac2
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2009-11-29 04:33:53 +0900

    had warnings. fix it.

--- src/bitboard_reversi_functions.cpp
+++ src/bitboard_reversi_functions.cpp
@@ -395,22 +395,22 @@ namespace Fujitalab
 
 			// 8x8 専用 (分割統治方を用いているため、4x4, 2x2にも応用できる)
 			x =
-				x << 1 & 0xAA00AA00AA00AA00ULL |
-				x >> 1 & 0x0055005500550055ULL |
-				x >> 8 & 0x00AA00AA00AA00AAULL |
-				x << 8 & 0x5500550055005500ULL ;
+				((x << 1) & 0xAA00AA00AA00AA00ULL) |
+				((x >> 1) & 0x0055005500550055ULL) |
+				((x >> 8) & 0x00AA00AA00AA00AAULL) |
+				((x << 8) & 0x5500550055005500ULL) ;
 			
 			x =
-				x << 2 & 0xCCCC0000CCCC0000ULL |
-				x >> 2 & 0x0000333300003333ULL |
-				x >> 16& 0x0000CCCC0000CCCCULL |
-				x << 16& 0x3333000033330000ULL ;
+				((x << 2) & 0xCCCC0000CCCC0000ULL) |
+				((x >> 2) & 0x0000333300003333ULL) |
+				((x >>16) & 0x0000CCCC0000CCCCULL) |
+				((x <<16) & 0x3333000033330000ULL) ;
 			
 			x =
-				x << 4 & 0xF0F0F0F000000000ULL |
-				x >> 4 & 0x000000000F0F0F0FULL |
-				x >>32 & 0x00000000F0F0F0F0ULL |
-				x <<32 & 0x0F0F0F0F00000000ULL ;
+				((x << 4) & 0xF0F0F0F000000000ULL) |
+				((x >> 4) & 0x000000000F0F0F0FULL) |
+				((x >>32) & 0x00000000F0F0F0F0ULL) |
+				((x <<32) & 0x0F0F0F0F00000000ULL) ;
 			return x;
 		}
 
